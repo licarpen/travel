9475a71718cbcfc454b40e9d324664e7788ea0aa
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-16 16:51:40 -0800

    added middleware for get woeid and itinerary items paths and tests

--- __tests__/itineraryItems.test.js
+++ __tests__/itineraryItems.test.js
@@ -31,7 +31,9 @@ describe('app routes', () => {
       .create({
         tripId: thaiTrip._id,
         activity: 'deep water soloing',
-        cost: 4
+        cost: 4,
+        city: 'Phuket',
+        woeid: 1226113
       });
   });
 
@@ -45,56 +47,18 @@ describe('app routes', () => {
       .send({
         tripId: thaiTrip._id,
         activity: 'deep water soloing',
-        cost: 4
+        cost: 4,
+        city: 'Phuket'
       })
       .then(res => {
         expect(res.body).toEqual({
-          tripId: thaiTrip._id,
+          tripId: thaiTrip.id,
           activity: 'deep water soloing',
+          city: 'Phuket',
           cost: 4,
           __v: 0,
-          _id: expect.any(String)
-        });
-      });
-  });
-  it('gets all itinerary items', async() => {
-    const itineraryItems = await ItineraryItem.create([
-      { tripId: thaiTrip._id, activity: 'deep water soloing', cost: 4 },
-      { tripId: thaiTrip._id, activity: 'thai massage', cost: 3 },
-      { tripId: thaiTrip._id, activity: 'eating curry', cost: 2 },
-    ]);
-    return request(app)
-      .get('/api/v1/itineraryItems')
-      .then(res => {
-        itineraryItems.forEach(item => {
-          expect(res.body).toContainEqual(JSON.parse(JSON.stringify(item)));
-        });
-      });
-  });
-  it('gets an itinerary item by id', () => {
-    return request(app)
-      .get(`/api/v1/itineraryItems/${thaiItineraryItem._id}`)
-      .then(res => {
-        expect(res.body).toEqual({
           _id: expect.any(String),
-          tripId: thaiTrip._id,
-          activity: 'deep water soloing',
-          cost: 4,
-          __v: 0
-        });
-      });
-  });
-  it('updates an itinerary item by id', () => {
-    return request(app)
-      .patch(`/api/v1/itineraryItems/${thaiItineraryItem._id}`)
-      .send({ cost: 2 })
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.any(String),
-          tripId: thaiTrip._id,
-          activity: 'deep water soloing',
-          cost: 2,
-          __v: 0
+          woeid: 1226113
         });
       });
   });
@@ -104,10 +68,12 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          tripId: thaiTrip._id,
+          tripId: thaiTrip._id.toString(),
           activity: 'deep water soloing',
           cost: 4,
-          __v: 0
+          city: 'Phuket',
+          __v: 0,
+          woeid: 1226113
         });
       });
   });
